4a5edad72041060da2be0b8f7813bedf19a85c12
Mochan Anil <mochan.anil@flyporter.com>
2025-12-10 21:58:34 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -1,41 +1,62 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, Dispatch, SetStateAction } from 'react';
 
-function getValue<T>(key: string, initialValue: T | (() => T)) {
-  if (typeof window === 'undefined') {
-    return initialValue instanceof Function ? initialValue() : initialValue;
-  }
-  try {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : (initialValue instanceof Function ? initialValue() : initialValue);
-  } catch (error) {
-    console.warn(`Error reading localStorage key “${key}”:`, error);
-    return initialValue instanceof Function ? initialValue() : initialValue;
-  }
-}
-
-export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-  const [storedValue, setStoredValue] = useState<T>(() => getValue(key, initialValue));
-
-  const setValue = useCallback((value: T | ((val: T) => T)) => {
+// See: https://usehooks-ts.com/react-hook/use-local-storage
+function useLocalStorage<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] {
+  // Get from local storage then
+  // parse stored json or return initialValue
+  const readValue = useCallback((): T => {
+    // Prevent build error "window is undefined" but keeps working
     if (typeof window === 'undefined') {
-      console.warn(`Tried to set localStorage key “${key}” even though no window was found`);
+      return initialValue;
     }
 
     try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      const item = window.localStorage.getItem(key);
+      return item ? (JSON.parse(item) as T) : initialValue;
     } catch (error) {
-      console.warn(`Error setting localStorage key “${key}”:`, error);
+      console.warn(`Error reading localStorage key “${key}”:`, error);
+      return initialValue;
     }
-  }, [key, storedValue]);
-  
+  }, [initialValue, key]);
+
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(readValue);
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue: Dispatch<SetStateAction<T>> = useCallback(
+    value => {
+      // Prevent build error "window is undefined" but keeps working
+      if (typeof window === 'undefined') {
+        console.warn(`Tried to set localStorage key “${key}” even though no window was found`);
+      }
+
+      try {
+        // Allow value to be a function so we have same API as useState
+        const newValue = value instanceof Function ? value(storedValue) : value;
+
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(newValue));
+
+        // Save state
+        setStoredValue(newValue);
+
+      } catch (error) {
+        console.warn(`Error setting localStorage key “${key}”:`, error);
+      }
+    },
+    [key, storedValue]
+  );
+
   useEffect(() => {
-    setStoredValue(getValue(key, initialValue));
-  }, [key, initialValue]);
+    setStoredValue(readValue());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-
-  return [storedValue, setValue] as const;
+  return [storedValue, setValue];
 }
+
+export { useLocalStorage };
